feed33e0472bb339690516f7ab021e27abb8ae7a
Justmide <oyediranolumide97@yahoo.com>
2024-12-10 19:42:15 +0100

    index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
       x.style.display = "block";
+    } else {
+      x.style.display = "none";
     }
   }
